1726aebdd3fc6dfe8440723812bef1f631572cab
Like Zheng <keithleaves@gmail.com>
2019-06-13 00:34:29 +0800

    update

--- scripts/cdn.js
+++ scripts/cdn.js
@@ -1,36 +1,39 @@
-'use strict'
+'use strict';
 
 function cdn(args) {
-  args = args.join(' ').split(',')
-  let img = args[0]
-  let title = args[1] || ''
-  return `<img src="${hexo.config.cdn}${this.slug}/${img}" alt="${title}">`
+  args = args.join(' ').split(',');
+  let img = args[0];
+  let id = args[1] || false;
+  let title = args[2] || '';
+  return id
+    ? `<img src="${hexo.config.gateway}${img}" alt="${title}">`
+    : `<img src="${hexo.config.cdn}${this.slug}/${img}" alt="${title}">`;
 }
 
-hexo.extend.tag.register('cdn', cdn)
+hexo.extend.tag.register('cdn', cdn);
 
 function fullImageCdn(args) {
-  args = args.join(' ').split(',')
-  var mixed = args[0].split('@')
-  var img = `${hexo.config.cdn}${this.slug}/${mixed[0]}`
-  var src = mixed[1] === 'lazy' ? '/images/loading.gif" data-original="' + img : img
-  var alt = args[1] || ''
-  var title = args[2] || ''
-  var width = args[3] || ''
+  args = args.join(' ').split(',');
+  var mixed = args[0].split('@');
+  var img = `${hexo.config.cdn}${this.slug}/${mixed[0]}`;
+  var src = mixed[1] === 'lazy' ? '/images/loading.gif" data-original="' + img : img;
+  var alt = args[1] || '';
+  var title = args[2] || '';
+  var width = args[3] || '';
 
   if (!img) {
-    hexo.log.warn('Image src can NOT be empty')
+    hexo.log.warn('Image src can NOT be empty');
   }
 
   var image = [
     `<span itemprop="image" itemscope itemtype="http://schema.org/ImageObject"><img itemprop="url image" src="${src}" class="full-image"`
-  ]
-  alt.length > 0 && image.push(`alt="${alt.trim()}"`)
-  title.length > 0 && image.push(`title="${title.trim()}"`)
-  width.length > 0 && image.push(`style="max-width: none; width:${width};"`)
-  image.push('/><meta itemprop="width" content="auto"/><meta itemprop="height" content="auto"/></span>')
+  ];
+  alt.length > 0 && image.push(`alt="${alt.trim()}"`);
+  title.length > 0 && image.push(`title="${title.trim()}"`);
+  width.length > 0 && image.push(`style="max-width: none; width:${width};"`);
+  image.push('/><meta itemprop="width" content="auto"/><meta itemprop="height" content="auto"/></span>');
 
-  return image.join(' ')
+  return image.join(' ');
 }
 
-hexo.extend.tag.register('cdnfi', fullImageCdn)
+hexo.extend.tag.register('cdnfi', fullImageCdn);

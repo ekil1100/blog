f2975052de72b83ccfe20c55d3d5e922a792f622
lastingman <keithleaves@gmail.com>
2020-09-21 01:46:33 +0800

    feat: add umami script

---
+++ scripts/umami.js
@@ -0,0 +1,6 @@
+process.env.NODE_ENV === 'production'
+  ? hexo.extend.injector.register(
+      'head_end',
+      `<script async defer data-website-id="51f01eff-19df-4867-bfd1-df2e7112edbe" src="https://u.lastingman.com/umami.js"></script>`
+    )
+  : null

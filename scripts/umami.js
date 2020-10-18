27fc0fe472495e197531d7d1c5ee1cdafc06f6ce
lastingman <keithleaves@gmail.com>
2020-10-18 23:08:31 +0800

    .

--- scripts/umami.js
+++ scripts/umami.js
@@ -1,6 +1,6 @@
 process.env.NODE_ENV !== 'development'
   ? hexo.extend.injector.register(
       'head_end',
-      `<script async defer data-website-id="51f01eff-19df-4867-bfd1-df2e7112edbe" src="https://u.lastingman.com/umami.js"></script>`
+      `<script async defer data-website-id="b4572a19-f8c0-49fa-9dd5-bc4b5cc9c226" src="https://u.lastingman.com/umami.js"></script>`
     )
   : null

c021343624a10d9d94a879080c1f690a79e90e53
Like <like@ekil.sh>
2023-04-26 18:29:24 +0800

    wip: adding contentlayer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,45 @@
-export default function Home() {
-    return <main></main>
+import Link from 'next/link'
+import { compareDesc, format, parseISO } from 'date-fns'
+import { allPosts } from 'contentlayer/generated'
+
+function PostCard(post: (typeof allPosts)[0]) {
+    return (
+        <div className='mb-6'>
+            <time dateTime={post.date} className='block text-sm text-slate-600'>
+                {format(parseISO(post.date), 'LLLL d, yyyy')}
+            </time>
+            <h2 className='text-lg'>
+                <Link
+                    href={post.url}
+                    className='text-blue-700 hover:text-blue-900'
+                >
+                    {post.title}
+                </Link>
+            </h2>
+        </div>
+    )
 }
+
+export const metadata = {
+    title: 'Contentlayer Blog Example',
+}
+
+function Home() {
+    const posts = allPosts.sort((a, b) =>
+        compareDesc(parseISO(a.date), parseISO(b.date)),
+    )
+
+    return (
+        <div className='mx-auto max-w-2xl py-16 text-center'>
+            <h1 className='mb-8 text-3xl font-bold'>
+                Contentlayer Blog Example
+            </h1>
+
+            {posts.map((post, idx) => (
+                <PostCard key={idx} {...post} />
+            ))}
+        </div>
+    )
+}
+
+export default Home
---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+import { withContentlayer } from 'next-contentlayer'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        appDir: true,
+    },
+}
+
+export default withContentlayer(nextConfig)

ec9dd44c6e3e54c6d55d59f152b0237d956620f3
Like <like@ekil.sh>
2023-07-11 08:07:02 +0800

    .

--- app/page.tsx
+++ app/page.tsx
@@ -20,10 +20,6 @@ function PostCard(post: (typeof allPosts)[0]) {
     )
 }
 
-export const metadata = {
-    title: 'Contentlayer Blog Example',
-}
-
 function Home() {
     const posts = allPosts.sort((a, b) =>
         compareDesc(parseISO(a.date), parseISO(b.date)),
@@ -31,9 +27,7 @@ function Home() {
 
     return (
         <div className='mx-auto max-w-2xl py-16 text-center'>
-            <h1 className='mb-8 text-3xl font-bold'>
-                Contentlayer Blog Example
-            </h1>
+            <h1 className='mb-8 text-3xl font-bold'>Like&apos;s Blog</h1>
 
             {posts.map((post, idx) => (
                 <PostCard key={idx} {...post} />
